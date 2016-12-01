6254a935c210c7b61d726cd9b510d05a6102e946
wangliang <wangliang@zhongzhihui.com>
2016-12-01 20:30:40 +0800

    wl

--- source/js/conf/index.js
+++ source/js/conf/index.js
@@ -9,6 +9,10 @@ define(function(require, exports, module) {
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
     var io = require('lib/core/1.0.0/io/request');
     var swiper = require('lib/plugins/swiper/3.1.2/swiper');
+    require('plugins/layer/layer');
+    //console.log(layer);
+    layer.msg('Hello layer');
+
     //轮播图
     var slider = new Slider('#jSlider', {
         lazyLoad: {
@@ -44,8 +48,21 @@ define(function(require, exports, module) {
     //师资团队滚动
     var teacherSwiper = new swiper('.swiper-container',{
         // 如果需要前进后退按钮
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
+        nextButton: '.arrow-right',
+        prevButton: '.arrow-left',
         slidesPerView: 3
+    });
+
+    //教学环境合作企业tab切换
+    $('.jTitle').on('click',function(){
+        var val = $(this).attr('data-value');
+        $(this).addClass('title-active').siblings().removeClass('title-active');
+        if(val === '0'){
+            $('.jTab1').hide();
+            $('.jTab0').show();
+        }else {
+            $('.jTab0').hide();
+            $('.jTab1').show();
+        }
     });
 });
