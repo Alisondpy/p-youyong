ed7adf18deedeb3c6aac7f732932317cbea0bada
wangliang <wangliang@zhongzhihui.com>
2016-12-02 10:53:35 +0800

    index compele

--- source/js/conf/index.js
+++ source/js/conf/index.js
@@ -65,12 +65,46 @@ define(function(require, exports, module) {
     });
 
     //弹出相框
+    function getAlums(photoId){
+        io.get('/p-youyong/source/api/index/index.json',{"pid":photoId},function(data){
+            if(data.error < 0){
+                box.error(data.msg);
+            }else {
+                try{
+                    var imgData = data.data;
+                    var title = imgData[0].imageName;
+                    var id = imgData[0].imageId;
+                    var imgList = [];
+                    $.each(imgData, function(i, n) {
+                        console.log(n);
+                        imgList[i] = {
+                            "alt":n.imageName,
+                            "pid":n.imageId,
+                            "src":n.imageUrl,
+                            "thumb":""
+                        }
+                    });
+                    var imgJson = {
+                        "title":title,
+                        "id":id,
+                        "start":0,
+                        "status":1,
+                        "data":imgList
+                    };
+                    layer.photos({
+                        photos: imgJson,
+                        anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+                    });
+                }catch (e){
+                    box.error('暂无数据');
+                }
+            }
+        },function(res){
+            box.error('网络错误');
+        },this);
+    };
     $('.jAlum').on('click',function(){
-        io.get('test/photos.json?v='+new Date, function(json){
-            layer.photos({
-                photos: json //格式见API文档手册页
-                ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机
-            });
-        });
+        var photoId = $(this).attr("data-id");
+        getAlums(photoId);
     });
 });
