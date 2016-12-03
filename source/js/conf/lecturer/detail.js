298ebfc386754732c95b2a7a5239d93289d9cdef
lvxiaonan <lvxiaonan@zhongzhihui.com>
2016-12-03 20:29:13 +0800

    donkey201612032029

--- source/js/conf/lecturer/detail.js
+++ source/js/conf/lecturer/detail.js
@@ -1,20 +1,27 @@
-/**
- * Created by wangLiang on 2016/11/30 0030.
- */
 define(function(require, exports, module) {
     'use strict';
     var $ = require('jquery');
     var box = require('lib/ui/box/1.0.1/box');
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
     var io = require('lib/core/1.0.0/io/request');
-    // var validate = require('lib/plugins/validation/1.15.1/jquery-validate');
-    // var message = require('lib/plugins/validation/1.15.1/localization/messages_zh');
+    require('lib/plugins/validation/1.15.1/jquery-validate');
+    require('lib/plugins/validation/1.15.1/localization/messages_zh');
+    var form = require('lib/core/1.0.0/utils/form');
+    var template = require('template');
     var jTinfo = $('.jTinfo');
     var tab1 = $('.jTab-l');
     var tab2 = $('.jTab-r');
     var jLi = $('.jLi');
-    var mask = $('.mod-mask');
-    
+    var mask = $('.jMod-mask');
+    var f = $('.f');
+
+    //模板加载
+    io.get('/path/to/file',function(data) {
+            var html = template('test',data);
+            docuemnt.getElementById('content').innerHTML = html;
+    });
+
+
     //图片懒加载
     var lazy = new Lazyload($('.jImg'), {
         mouseWheel: true,
@@ -52,20 +59,58 @@ define(function(require, exports, module) {
 
     $('#btn').on('click',function(){
         io.post();
+        cvalidate();
+        var formData = form.serializeForm(cvalidate());
+            console.log(formData);
     });
 
-    // function cvalidate(){
-    //     $('#cvalidate').validate({
-    //         rules:{
-    //             cname:{
-    //                 required: true,
-    //                 minlength:1
-    //             }
-    //             ctel:{
-    //                 required:true;
-    //             }
-    //         }
-    //     })
-    // }
 
+
+        $().ready(function(){
+           $('#cvalidate').validate({
+             onfocusout: function(element){
+                $(element).valid();
+            },
+            debug:true,
+            rules:{
+                cname:{
+                    required:true,
+                    minlength:2
+                },
+                ctel:{
+                    required:true,
+
+                },
+                ccompany:{
+                    required:true,
+                },
+                cneed:{
+                    required:true,
+
+                }
+            },
+            messages:{
+                cname:{
+                    required:"",
+                    minlength:""
+                },
+                ctel:{
+                    required:""
+
+                },
+                ccompany:{
+                    required:""
+                },
+                cneed:{
+                    required:""
+
+                }
+            }
+        }) 
+    })
+        // jQuery.validator.addMethod('phone',function(value, element){
+        //     var tel = /0?(13|14|15|18)[0-9]{9}/;
+        //     return this.optional(element) || (tel.test(value));
+        // },'请填写正确的手机号码');
+        
 });
