57f2085351a70d3841cac9ee055186d744fd3a28
wangliang <wangliang@zhongzhihui.com>
2016-12-03 18:16:10 +0800

    wl201612031816

--- source/js/conf/course/index.js
+++ source/js/conf/course/index.js
@@ -7,13 +7,30 @@ define(function(require, exports, module) {
     var box = require('lib/ui/box/1.0.1/box');
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
     var io = require('lib/core/1.0.0/io/request');
+    var template=require("template");
+    var lazy;
 
-    //图片懒加载
-    var lazy = new Lazyload($('.jImg'), {
-        mouseWheel: true,
-        effect: 'fadeIn',
-        snap: true
-    });
+    function getLists(url,data,tmpEl,htmEl){
+        io.get(url,{"data":data},function(res){
+            if(!$.isEmptyObject(res.data) && res.data.list.length > 0){
+                var html = template(tmpEl,res.data);
+                document.getElementById(htmEl).innerHTML = html;
+            }else {
+                document.getElementById(htmEl).innerHTML = "<div style='color: #000;'>暂无数据</div>";
+            }
+
+            //图片懒加载
+            lazy = new Lazyload($('.jImg'), {
+                mouseWheel: true,
+                effect: 'fadeIn',
+                snap: true
+            });
+        },function(res){
+            document.getElementById(htmEl).innerHTML = "<div style='color: #000;'>请求超时请重试！<a href=''>刷新</a></div>";
+        });
+    }
+
+    getLists('/p-youyong/source/api/course/tab0.json','系列课','tab0','jTab0');
 
     //课程类型切换
     $('#jCourseType').on('click','.nav li',function(){
@@ -23,7 +40,7 @@ define(function(require, exports, module) {
         var type = $(this).find('a').attr('data-type');
         if(type === '1'){
             $('#jType1').show();
-            $('#jType2').hide();
+            $('#jType2').show();
             $('#jSubNav').show();
             $('#jTab0').hide();
             $('#jTab1').show();
@@ -32,8 +49,11 @@ define(function(require, exports, module) {
             if(!$('#jType0').hasClass('ui-nav-border')){
                 $('#jType0').addClass('ui-nav-border');
             }
-            $('#jType1').removeClass('ui-nav-border');
+            if(!$('#jType1').hasClass('ui-nav-border')){
+                $('#jType1').addClass('ui-nav-border');
+            }
             $('#jType2').removeClass('ui-nav-border');
+            getLists('/p-youyong/source/api/course/tab1.json','点播课','tab1','jTab1');
         }else if(type === '2'){
             $('#jType1').show();
             $('#jType2').show();
@@ -49,6 +69,7 @@ define(function(require, exports, module) {
                 $('#jType1').addClass('ui-nav-border');
             }
             $('#jTab2').removeClass('ui-nav-border');
+            getLists('/p-youyong/source/api/course/tab2.json','直播课','tab2','jTab2');
         }else {
             $('#jType1').hide();
             $('#jType2').hide();
@@ -60,16 +81,17 @@ define(function(require, exports, module) {
             $('#jType0').removeClass('ui-nav-border');
             $('#jType1').removeClass('ui-nav-border');
             $('#jType2').removeClass('ui-nav-border');
+            getLists('/p-youyong/source/api/course/tab0.json','系列课','tab0','jTab0');
         }
         lazy.update();
     });
     //点播课和直播课效果
-    $('.jList').mouseenter(function(){
+    $('.jLists').on('mouseenter','.jList',function(){
         var titleLen = $(this).find('.jTitle').text().length;
         if(titleLen > 13){
             $(this).find('.jModLabel').stop(true,false).slideUp();
         }
-    }).mouseleave(function(){
+    }).on('mouseleave','.jList',function(){
         var titleLen = $(this).find('.jTitle').text().length;
         if(titleLen > 13){
             $(this).find('.jModLabel').stop(true,false).slideDown();
