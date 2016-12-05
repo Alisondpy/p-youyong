a6e2a7ca9b7f747d665973df12d2ac1641642292
zhanglihong <zhanglihong@zhongzhihui.com>
2016-12-05 09:34:16 +0800

    AAA

--- source/js/conf/uc/questions-answers.js
+++ source/js/conf/uc/questions-answers.js
@@ -1,13 +1,13 @@
 /**
  * Created by temulun on 2016/12/04 14:18.
  */
-define(function(require, exports, module){
-	'use strict';
-	var $ = require('jquery');
+define(function(require, exports, module) {
+    'use strict';
+    var $ = require('jquery');
     var box = require('lib/ui/box/1.0.1/box');
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
     var io = require('lib/core/1.0.0/io/request');
-    var template=require("template");
+    var template = require("template");
     var lazy;
     lazy = new Lazyload($('.jImg'), {
         mouseWheel: true,
@@ -15,43 +15,43 @@ define(function(require, exports, module){
         snap: true
     });
     //发表评论
-    $('#jBtnPublish').on('click', function () {
-    	box.ok('评论成功');
+    $('#jBtnPublish').on('click', function() {
+        box.ok('评论成功');
     });
     //发布新问题
-    $('#jBtnNewQuestions').on('click', function () {
-    	box.ok('new questions publish success');
+    $('#jBtnNewQuestions').on('click', function() {
+        box.ok('new questions publish success');
     });
     //回复评论
-    $('#jReplyList').on('click', function (e) {
-    	if($(e.target).is('.jItemsReply')) {
-    		box.ok($(e.target).text())
-    	}
-    })
-    //赞和回复
-    $('.jClkRefavour').on('click', function (e) {
-    	if($(e.target).is('.jClkFavour')) {
-    		$(e.target).addClass('favour');
-    	}else if( $(e.target).is('.jClkResponse') ) {
-    		box.ok('实现什么效果？')
-    	}
-    })
-    //点击查看全部评论
-    $('#jMoreComment').on('click', function(){
-         $(this).hide();
-         insertItems($PAGE_DATA['questions'], '查看全部', 'jReply', 'jReplyList');
-     });
+    $('#jReplyList').on('click', function(e) {
+            if ($(e.target).is('.jItemsReply')) {
+                box.ok($(e.target).text())
+            }
+        })
+        //赞和回复
+    $('.jClkRefavour').on('click', function(e) {
+            if ($(e.target).is('.jClkFavour')) {
+                $(e.target).addClass('favour');
+            } else if ($(e.target).is('.jClkResponse')) {
+                box.ok('实现什么效果？')
+            }
+        })
+        //点击查看全部评论
+    $('#jMoreComment').on('click', function() {
+        $(this).hide();
+        insertItems($PAGE_DATA['questions'], '查看全部', 'jReply', 'jReplyList');
+    });
     //查看全部评论
-    function insertItems(url, data, tmpEl, htmEl){
-        io.get(url, {"data":data}, function(res){
-        	var data = res.data;
-            if(!$.isEmptyObject(data) && data.list.length > 0) {
+    function insertItems(url, data, tmpEl, htmEl) {
+        io.get(url, { "data": data }, function(res) {
+            var data = res.data;
+            if (!$.isEmptyObject(data) && data.list.length > 0) {
                 var html = template(tmpEl, data);
                 document.getElementById(htmEl).innerHTML += html;
-            }else if(res.error < 0) {
+            } else if (res.error < 0) {
                 box.warn('有问题')
-            }else {
-            	document.getElementById(htmEl).innerHTML = '<div class="ui-empty-list"><div class="isema isema-box"></div><div class="txt">暂时没有相关内容</div></div>';
+            } else {
+                document.getElementById(htmEl).innerHTML = '<div class="ui-empty-list"><div class="isema isema-box"></div><div class="txt">暂时没有相关内容</div></div>';
             }
 
             //图片懒加载
@@ -60,23 +60,24 @@ define(function(require, exports, module){
                 effect: 'fadeIn',
                 snap: true
             });
-        },function(error){
+        }, function(error) {
             document.getElementById(htmEl).innerHTML = '<div style="color: #000;">请求超时请重试！<a href="">刷新</a></div>';
         });
     }
     insertAlist($PAGE_DATA['questions'], '发布新问题', 'jAlist', 'jRightQuestions')
+
     function insertAlist(url, data, tmpEl, htmEl) {
-    	io.get(url, {"data":data}, function(res){
-        	var data = res.data;
-            if(!$.isEmptyObject(data) && data.alist.length > 0) {
+        io.get(url, { "data": data }, function(res) {
+            var data = res.data;
+            if (!$.isEmptyObject(data) && data.alist.length > 0) {
                 var html = template(tmpEl, data);
                 document.getElementById(htmEl).innerHTML += html;
-            }else if(res.error < 0) {
+            } else if (res.error < 0) {
                 box.warn('有问题');
-            }else {
-            	document.getElementById(htmEl).innerHTML = '<div class="ui-empty-list"><div class="isema isema-box"></div><div class="txt">暂时没有相关内容</div></div>';
+            } else {
+                document.getElementById(htmEl).innerHTML = '<div class="ui-empty-list"><div class="isema isema-box"></div><div class="txt">暂时没有相关内容</div></div>';
             }
-        },function(error){
+        }, function(error) {
             document.getElementById(htmEl).innerHTML = '<div style="color: #000;">请求超时请重试！<a href="">刷新</a></div>';
         });
     }
---
+++ source/js/conf/uc/register.js
@@ -0,0 +1,20 @@
+// define(function(require, exports, module) {
+// 	'use strict';
+//     var $ = require('jquery');
+//     var box = require('lib/ui/box/1.0.1/box');
+//     // require('lib/plugins/validation/1.15.1/jquery-validate');
+//     // require('lib/plugins/validation/1.15.1/localization/messages_zh');
+//     // var validate = require('lib/plugins/validation/1.15.1/jquery-validate');
+//     // var validationMessages = require('lib/plugins/validation/1.15.1/localization/messages_zh');
+//     //console.log(validate())
+//     /*$('#signupForm').validate({
+//     	rules:{
+//     		name:{
+//     			required:true,
+//     			minlength:2,
+//     			maxlength:5
+//     		}
+//     	}
+//     })*/
+// })
+alert()
