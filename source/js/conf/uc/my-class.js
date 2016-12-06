751007dd3f5d926b8428c4446bd043796a2c7a8f
zhanglihong <zhanglihong@zhongzhihui.com>
2016-12-06 21:12:37 +0800

    AAA

--- source/js/conf/uc/my-class.js
+++ source/js/conf/uc/my-class.js
@@ -8,24 +8,35 @@ define(function(require, exports, module) {
 		var Accordion = function(el, multiple) {
 			this.el = el || {};
 			this.multiple = multiple || false;
-
 			// Variables privadas
 			var links = this.el.find('.link');
 			// Evento
 			links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+
 		}
 		Accordion.prototype.dropdown = function(e) {
 			var $el = e.data.el,
 				$this = $(this),
 				$next = $this.next();
 			$next.slideToggle();
-			$this.parent().toggleClass('open').siblings().removeClass('open');
-
-			if (!e.data.multiple) {
-				$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-			};
+			if($this.parent().children().length>1) {
+				$this.parent().toggleClass('spread').siblings().removeClass('open');
+			} else {
+				$this.parent().toggleClass('open').siblings().removeClass('open');
+			}
+			if($this.parent().siblings('spread')){
+				if (!e.data.multiple) {
+					$el.find('.submenu').not($next).slideUp().parent().removeClass('spread');
+				} else if($this.parent().children().length>1) {
+					$this.parent().siblings().removeClass('open');
+				}
+			}
 		}
 		var accordion = new Accordion($('#accordion'), false);
+		$('#accordion .sublist').on('click', function () {
+			$(this).addClass('stamp').siblings().removeClass('stamp');
+		});
+
 	});
 
 
