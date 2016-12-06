836e7b7613be78f1f090e9aa866a096df1652743
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-06 17:55:38 +0800

    N/A

--- source/js/module/login-status/1.0.0/login-status.js
+++ source/js/module/login-status/1.0.0/login-status.js
@@ -9,15 +9,89 @@ define(function(require, exports, module) {
     function LoginStatus(options) {
         var _this = this;
         var defaults = {
-            url: '',
-            data: null
+            el: '#jLoginStatus',
+            ajax: {
+                type: 'get', //'get'|'post'|'jsonp'
+                url: ($PAGE_DATA && $PAGE_DATA['getUserInfo']) || '', // required 获取用户登录信息
+                data: null //获取用户登录信息接口参数
+            },
+            userCenterUrl: ($PAGE_DATA && $PAGE_DATA['userCenterUrl']) || 'javascript:;', //用户中心url
+            loginOutUrl: ($PAGE_DATA && $PAGE_DATA['loginOutUrl']) || 'javascript:;', //登出url
+            //下拉菜单配置
+            menuList: [{
+                title: '编辑资料',
+                url: ($PAGE_DATA && $PAGE_DATA['editUserInfoUrl']) || 'javascript:;'
+            }, {
+                title: '学习中心',
+                url: ($PAGE_DATA && $PAGE_DATA['learningCenterUrl']) || 'javascript:;'
+            }]
         };
-        _this.el = $(selector);
         _this.options = $.extend(true, {}, defaults, options);
+        if (!_this.options.ajax.url) {
+            throw new Error('the param [options.ajax.url] is required.');
+        }
+        _this.el = $(options.selector);
         _this._init();
     };
+
     LoginStatus.prototype._init = function() {
-        var _this = this;
+        var _this = this,
+            options = _this.options,
+            ajax = options.ajax;
+        io[ajax.type](ajax.url, ajax.data, function(data) {
+            _this.el.html(_this._getLoginedHtml(data.data));
+            _this._initEvent();
+        }, function(data) {
+            //获取登录信息失败
+        })
     };
+
+    LoginStatus.prototype._initEvent = function() {
+        var _this = this,
+            isMoveing = false,
+            builder = build.build(_this.el[0]),
+            userName = builder.get('userName'),
+            tipsMenu = builder.get('tipsMenu');
+
+        userName.on('mouseenter', function() {
+
+        });
+        userName.on('mouseleave', function() {
+
+        });
+        tipsMenu.on('mouseenter', function() {
+
+        });
+        tipsMenu.on('mouseleave', function() {
+
+        });
+    };
+
+    LoginStatus.prototype._getLoginedHtml(data) {
+        var _this = this,
+            options = _this.options,
+            menuList = options.menuList;
+        var str = '';
+        str += '<ul class="logined clearfix" node-type="logined">';
+        str += '    <li class="item">';
+        str += '        <span>您好，</span>';
+        str += '    </li>';
+        str += '    <li class="item tips-menu-box">';
+        str += '        <a href="' + options.userCenterUrl + '" class="user-name txt-overflow" node-type="userName">' + data.nick + '</a>';
+        str += '        <div class="tips-menu" node-type="tipsMenu">';
+        str += '            <div class="arrow"><i></i><b></b></div>';
+        str += '            <ul class="tips-menu-list">';
+        for (var i = 0, len = menuList.lenth; i < len; i++) {
+            str += '            <li class="tips-menu-item"><a href="' + menuList[i].url + '">' + menuList[i].title + '</a></li>';
+        }
+        str += '            </ul>';
+        str += '        </div>';
+        str += '    </li>';
+        str += '    <li class="item">';
+        str += '        <a href="' + options.loginOutUrl + '" class="btn">退出</a>';
+        str += '    </li>';
+        str += '</ul>';
+        return str;
+    }
     module.exports = LoginStatus;
 });
