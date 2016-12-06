3f459e9bbe3941422bda089ba09b882f4361a0c0
lvxiaonan <lvxiaonan@zhongzhihui.com>
2016-12-06 09:15:15 +0800

    donkey20161206915

--- source/js/conf/lecturer/detail.js
+++ source/js/conf/lecturer/detail.js
@@ -3,10 +3,9 @@ define(function(require, exports, module) {
     var $ = require('jquery');
     var box = require('lib/ui/box/1.0.1/box');
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
+    require('plugins/validator/1.0.0/validator');
     var Tab = require('lib/ui/tab/1.0.0/tab');
     var io = require('lib/core/1.0.0/io/request');
-    require('lib/plugins/validation/1.15.1/jquery-validate');
-    require('lib/plugins/validation/1.15.1/localization/messages_zh');
     var template = require('template');
     var form = require('lib/core/1.0.0/utils/form'); 
     var jTinfo = $('.jTinfo');
@@ -14,14 +13,13 @@ define(function(require, exports, module) {
     var tab2 = $('.jTab-r');
     var jLi = $('.jLi');
     var mask = $('.jMod-mask');
-    // var f = $('.f');
 
     var jTab = $('#jTab');
     var tab = new Tab(jTab);
     console.log(tab);
-    // tab.on('click',function(){
-    //     console.log(0);
-    // })
+    tab.on('click',function(){
+        console.log(0);
+    })
        
     
      // tab.setCurrent(1);
@@ -72,13 +70,10 @@ define(function(require, exports, module) {
 
     $('#btn').on('click',function(){
         io.post();
-        cvalidate();
-        var formData = form.serializeForm(cvalidate());
-            console.log(formData);
     });
 
 
-
+    //关于遮罩层上面的的验证。
         $().ready(function(){
            $('#cvalidate').validate({
              onfocusout: function(element){
@@ -88,9 +83,8 @@ define(function(require, exports, module) {
             rules:{
                 cname:{
                     required:true,
-                    minlength:2
                 },
-                ctel:{
+                phone:{
                     required:true,
                     minlength:11
                 },
@@ -107,11 +101,6 @@ define(function(require, exports, module) {
                     minlength:"姓名不合法",
                     isname:"孩子长点心吧。你的名字都能写错？"
                 },
-                ctel:{
-                    required:"请填写手机号",
-                    isphonenum:"请填写正确的手机号码"
-
-                },
                 ccompany:{
                     required:"请填写公司的名称"
                 },
@@ -122,12 +111,10 @@ define(function(require, exports, module) {
             }
         }) 
     })
-        jQuery.validator.addMethod('isphonenum',function(value, element){
-            var tel = /0?(13|14|15|18)[0-9]{9}/;
-            return this.optional(element) || (tel.test(value));
-        });
         jQuery.validator.addMethod('isname',function(value, element){
             var name = /[\u4E00-\u9FA5]{2,5}(?:·[\u4E00-\u9FA5]{2,5})*/;
             return this.optional(element) || (name.test(value));
         });
+
+        $.required.text().animate().slideUp();
 });
