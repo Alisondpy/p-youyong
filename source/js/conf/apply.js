359b90bfebff3a7ef999d49ef15bf51874e3cceb
zhanglihong <zhanglihong@zhongzhihui.com>
2016-12-09 10:48:06 +0800

    AAA

--- source/js/conf/apply.js
+++ source/js/conf/apply.js
@@ -7,87 +7,62 @@ define(function(require, exports, module) {
     var template = require("template");
     var jContainer = $('#jContainer');
     var form = require('lib/core/1.0.0/utils/form');
-      /*box.loadUrl('http://www.baidu.com', {
-                data: { t: +new Date },
-                content: '加载中',
-                success: function(res) {
-                    console.log(res);
-                    alert(JSON.stringify(res));
-                }
-            });*/
-    // var data = null;
 
     var ucData ;
     var handshake = {
         init:function () {
             io.get($PAGE_DATA['getInfo'], function(data) {
-                // console.log(data);
                 if (data.error < 0) {
                     box.error(data.msg);
                 } else {
                     try {
                         data = data.data;
-                        // ucData = {
-                        //     'userid': data.userId,
-                        //     'companyId':data.companyId,
-                        //     'activityId':data.activityId,
-                        //     'userId':data.userId,
-                        //     'realname':data.realname,
-                        //     'mobile':data.mobile,
-                        //     'email':data.email,
-                        //     'wechat':data.wechat,
-                        //     'qq':data.qq,
-                        //     'school':data.school
-                        // };
-                        jContainer.html(template('jForm', data));
-                        console.log(form.serializeForm('#jSigninForm'));
+                        form.setFormData('#jSigninForm',data);
                     } catch (e) {
                         box.error('暂无数据');
                     }
                 }
             }, function(res) {
                 box.error('网络错误');
-            }, this);
+            });
         },
         handle:function (data) {
-            // console.log(ucData)
-            var ucData = $('#jSigninForm').serialize();
-            io.get($PAGE_DATA['getInfo'], {data:ucData} ,function(data){
-                // alert()
-            })
+            var ucData = form.serializeForm('#jSigninForm');
+            io.get($PAGE_DATA['getInfo'], ucData ,function(data){
+                box.ok('提交成功')
+            },function(res) {
+                box.error('网络错误,请重试');
+            }, this)
         }
     };
     handshake.init();
-    $().ready(function(){
-        $('#jSigninForm').validate({
-            //debug:true,
-            rules: {
-                admin: {
-                    required: true,
-                    admin:true
-                },
-                mobile:{
-                    required: true,
-                    mobile:true
-                },
-                email:{
-                    required: true,
-                    email:true
-                },
-                scholl:{
-                    required: true,
-                },
-                qq:{
-                    qq:true
-                },
-                wechat:{
-                    wechat:true
-                }
+    $('#jSigninForm').validate({
+        rules: {
+            realname: {
+                required: true,
+                realname:true
             },
-            submitHandler:function(form){
-                handshake.handle();
+            mobile:{
+                required: true,
+                mobile:true
+            },
+            email:{
+                required: true,
+                email:true
+            },
+            scholl:{
+                required: true,
+            },
+            qq:{
+                qq:true
+            },
+            wechat:{
+                wechat:true
             }
-        })
-    
+        },
+        submitHandler:function(form){
+            handshake.handle();
+        }
     })
+
 });
