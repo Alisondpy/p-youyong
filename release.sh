886f3dfcf1dd6fd3c31bd07b8053f4b4d4035cc1
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-11 17:47:15 +0800

    release@16-12-11 12:12:1481449635

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo 'start release';
 git add .
-git commit -m "release@ `date "+%y-%m-%d %h:%m:%s"` "
+git commit -m "release@`date "+%y-%m-%d %h:%m:%s"`"
 git push origin release
+ssh apps:apps@192.168.20.22
+cd /apps/data/web/working/s1.zhongzhihui.com/p-youyong
+git pull origin release
