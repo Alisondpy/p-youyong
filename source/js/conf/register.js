f65ce36387c0c24f99c83d19403911ef8b3fc42b
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-12 12:16:56 +0800

    N/A

--- source/js/conf/register.js
+++ source/js/conf/register.js
@@ -24,16 +24,16 @@ define(function(require, exports, module) {
     //手机号正确才允许点击验证码
     $.validator.addMethod("lms", function(value, element, param) {
         var mobile = /^0?(13[0-9]|15[0-9]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
-        if(mobile.test(value)){
+        if (mobile.test(value)) {
             //修改点击提交触发验证,恢复验证码按钮色彩的功能
             //change修改提交时触发验证手机号的bug
-            if($(".jsVerifyCode").hasClass("change")) {
+            if ($(".jsVerifyCode").hasClass("change")) {
 
-            }else{
+            } else {
                 $(param).removeClass("ui-btn-disable");
             }
             return this.optional(element) || true;
-        }else{
+        } else {
             $(param).addClass("ui-btn-disable");
             return false;
         }
@@ -47,91 +47,86 @@ define(function(require, exports, module) {
     }, "请输入动态码");
 
     //验证成功时执行方法
-    function validateSuccess(url,formRes){
+    function validateSuccess(url, formRes) {
         var formData = form.serializeForm(formRes);
-        var refer = document.referrer;//来源页面
+        var refer = document.referrer; //来源页面
         formData.returnUrl = encodeURI(refer);
-        io.get(url,formData,function(res){
+        io.get(url, formData, function(res) {
             box.ok("注册成功");
             refer = decodeURI(res.data.returnUrl);
             alert(refer);
             //等待1秒跳转页面
-            setTimeout(function(){
+            setTimeout(function() {
                 //注册成功,返回来源页面或登陆页面
                 //登陆页url
                 location.href = refer;
-            },1000);
-        },function(res){
-            if(res.msg){
-                box.error(res.msg);
-            }else{
-                box.error('网络错误');
-            }
-
+            }, 1000);
+        }, function(res) {
+            box.error((res && res.msg) || '网络错误，请重试！');
         });
     }
 
     //注册页面验证
     $("#jsRightSignin").validate({
         rules: {
-            mobile:{
+            mobile: {
                 lms: ".jsVerifyCode",
                 required: true,
-                mobile:true
+                mobile: true
             },
-            email:{
+            email: {
                 required: true,
-                email:true
+                email: true
             },
-            vierfyCode : {
-                required : true,
-                minlength : 4
+            vierfyCode: {
+                required: true,
+                minlength: 4
             },
-            password : {
-                required : true,
-                minlength : 6
+            password: {
+                required: true,
+                minlength: 6
             }
         },
-        messages : {
-            mobile:{
+        messages: {
+            mobile: {
                 required: "请输入手机号",
-                mobile:"请输入正确的手机号码"
+                mobile: "请输入正确的手机号码"
             },
-            vierfyCode : {
-                required : "请输入动态码",
-                minlength : ""
+            vierfyCode: {
+                required: "请输入动态码",
+                minlength: ""
             },
-            email:{
+            email: {
                 required: "请输入邮箱",
-                email:"请输入正确的邮箱"
+                email: "请输入正确的邮箱"
             },
-            password : {
-                required : "请输入密码",
-                minlength : "密码长度不小于6位,且不包含空格"
+            password: {
+                required: "请输入密码",
+                minlength: "密码长度不小于6位,且不包含空格"
             }
         },
         //失去焦点校验
-        onfocusout: function(element){
+        onfocusout: function(element) {
             $(element).valid();
         },
-        errorPlacement : function(error,element){
+        errorPlacement: function(error, element) {
             //验证码特殊结构,修改错误信息放置位置
-            if(element.attr("id") === "jDynamic"){
+            if (element.attr("id") === "jDynamic") {
                 error.appendTo(element.parents(".item"));
                 $(element).parent().addClass("error-red");
                 $(element).parents(".item").addClass("error-red");
-            }else {
+            } else {
                 error.appendTo(element.parent());
                 $(element).parent().addClass("error-red");
             }
         },
         success: function(label) {
             //验证码特殊结构,修改错误信息放置位置
-            if(label.attr("id") === "jDynamic-error"){
+            if (label.attr("id") === "jDynamic-error") {
                 label.html("&nbsp;").addClass("checked");
                 $(label).parent().removeClass("error-red");
                 $(label).siblings().children("label").removeClass("error-red");
-            }else {
+            } else {
                 label.html("&nbsp;").addClass("checked");
                 label.parent().removeClass("error-red");
             }
@@ -144,41 +139,37 @@ define(function(require, exports, module) {
     });
 
     //获取验证码
-    $(".jsVerifyCode").on("click",function(){
+    $(".jsVerifyCode").on("click", function() {
         var verifyCode = $(this);
         //获取阶段直接返回
-        if(verifyCode.hasClass("ui-btn-disable")){
+        if (verifyCode.hasClass("ui-btn-disable")) {
             return false;
         }
         //发送ajax请求
-        io.get($PAGE_DATA['code'],{mobile : $("#jName").val()},
-            function(res){
+        io.get($PAGE_DATA['code'], { mobile: $("#jName").val() },
+            function(res) {
                 //成功后的回调
-                console.log({mobile : $("#jName").val()});
+                console.log({ mobile: $("#jName").val() });
 
-            },function(res){
-                //fail
-                if(res.msg){
-                    box.error(res.msg);
-                }else{
-                    box.error('网络错误');
-                }
+            },
+            function(res) {
+                box.error((res && res.msg) || '网络错误，请重试！');
             });
         //验证码循环
         var count = 60;
         //class:change ==== 修改点击提交触发验证,触发验证手机号,恢复验证码按钮色彩的bug
         verifyCode.val(count).addClass("ui-btn-disable change");
-        var time = setInterval(function(){
-            if(count > 1){
-                count --;
+        var time = setInterval(function() {
+            if (count > 1) {
+                count--;
                 verifyCode.val(count);
-            }else{
+            } else {
                 verifyCode.removeClass("ui-btn-disable change").val("获取验证码");
                 clearInterval(time);
             }
-        },1000);
+        }, 1000);
     });
-    $(".jPopBtn").on("click",function(){
+    $(".jPopBtn").on("click", function() {
         box.loadUrl($PAGE_DATA['popUrl'], {
             title: '个人注册协议',
             autoRelease: false,
