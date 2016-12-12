0764e3506a0215c37e81122abbfb4fd65773000d
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-12 09:37:36 +0800

    release@16-12-12 12:12:1481506656

--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo 'start copy dist'
+cp -r /app/data/www/working/s1.zzhstatic.com/p-youyong/dist /app/data/www/working/s1.zzhstatic.com-release/p-youyong/dist 
 echo 'start release';
 git add .
 git commit -m "release@`date "+%y-%m-%d %h:%m:%s"`"
 git push origin release
+echo 'done'
 #ssh apps@192.168.20.22
 #cd /apps/data/web/working/s1.zhongzhihui.com/p-youyong
 #git pull origin release
