48b415265aceecdefd95092473daf6d7159c4b37
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-12 15:49:20 +0800

    N/A

--- source/js/module/login-status/1.0.0/login.js
+++ source/js/module/login-status/1.0.0/login.js
@@ -1,26 +1,29 @@
 define(function(require, exports, module) {
     'use strict';
     var cookie = require('lib/core/1.0.0/io/cookie');
-    //用户nick存储表示
+    //用户nick存储表示，这个版本后台暂时支持
     var NICK_NAME = '_nick';
+    //登陆标示
+    var UI = '_ui_';
 
     var LOGIN_URL = ($PAGE_DATA && $PAGE_DATA['LOGIN_URL']) || '';
+    var nick = ($PAGE_DATA && $PAGE_DATA[NICK_NAME]) || null;
 
     //获取用户昵称
     exports.getNick = function() {
-        return cookie(NICK_NAME);
+        return nick;
     }
 
     //判断用户是否登陆
     exports.isLogin = function() {
-        return cookie(NICK_NAME) ? true : false;
+        return cookie(UI) ? true : false;
     }
 
     //从哪里来到哪里去
     exports.login = function(returnUrl) {
         if (LOGIN_URL) {
             if (returnUrl) {
-                returnUrl = '?returnUrl=' + returnUrl;
+                returnUrl = '?returnUrl=' + decodeURIComponent(returnUrl);
             } else {
                 returnUrl = '';
             }
