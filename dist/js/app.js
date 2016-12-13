5adc52690802b88d77f91d9d52d5b852f73e9848
jiangchaoyi <jiangchaoyi@bubugao.com>
2016-12-13 22:31:36 +0800

    release@16-12-13 12:12:1481639496

--- dist/js/app.js
+++ dist/js/app.js
@@ -4455,4 +4455,4 @@ define("app", function() {});
     }, t.fx.stop = function() {
         r = !1;
     });
-}(window, jQuery, "requestAnimationFrame");require.config({urlArgs : "f5224f00a1"});
+}(window, jQuery, "requestAnimationFrame");require.config({urlArgs : "502f73cc0e"});
