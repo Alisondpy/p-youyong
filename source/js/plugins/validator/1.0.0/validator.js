e8d4f0031b14bb6f9b1fc5d734f82955ea760fe6
zhanglihong <zhanglihong@zhongzhihui.com>
2016-12-13 22:10:17 +0800

    tml2210

--- source/js/plugins/validator/1.0.0/validator.js
+++ source/js/plugins/validator/1.0.0/validator.js
@@ -107,9 +107,9 @@ define(function(require, exports, module) {
         }
     }, {
         name: 'password',
-        text: '请输入正确的密码',
+        text: '请输入6-16位密码，区分大小写，不能使用空格！',
         func: function(v, elem) {
-            return this.optional(elem) ||  /^(?!\d{1,8}$)\S{6,16}$/.test(v);
+            return this.optional(elem) ||  /^[\S]{6,16}$/.test(v);
         }
     }];
 
