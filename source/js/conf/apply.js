400e0078805248aeea9aa0694466b812feec2cdc
dengpeiyun <dengpeiyun@zhongzhihui.com>
2016-12-15 10:39:31 +0800

    dpy15:10:40

--- source/js/conf/apply.js
+++ source/js/conf/apply.js
@@ -11,13 +11,8 @@ define(function(require, exports, module) {
         handle:function () {
             var ucData = form.serializeForm('#jSigninForm');
             io.post($PAGE_DATA['getInfo'], $.extend({activityId:$PAGE_DATA['activityId']},ucData) ,function(data){
-                //isSubmit = false;
-                var topBox = box.get(window);
                 box.ok('恭喜您，报名成功！')
-                setTimeout(function(){
-                    window.top.window.location.reload();
-                    topBox.hide();
-                },2000);
+                window.top && window.top.window.location.reload();
             },function(res) {
                 box.error(res.msg || '网络错误,请重试');
             },jMSubBtn[0]);
