3b51922cb635f0b42d7847fe85a70601bf6137e3
wangliang <wangliang@zhongzhihui.com>
2016-12-18 18:22:35 +0800

    wlssss

--- source/js/conf/news/news-detail.js
+++ source/js/conf/news/news-detail.js
@@ -1,7 +1,13 @@
 define(function(require, exports, module) {
     'use strict';
     var $ = require('jquery');
+    var io = require('lib/core/1.0.0/io/request');
+    var cookie = require("lib/core/1.0.0/io/cookie");
+    var Login = require('module/login-status/1.0.0/login');
     var Lazyload = require('lib/plugins/lazyload/1.9.3/lazyload');
+    var box = require('lib/ui/box/1.0.1/crossbox');
+    //赞效果
+    var Praise = require('module/praise/1.0.0/praise');
     var lazy;
     var jHotTrain = $("#jHotTrain");
     //图片懒加载
@@ -26,7 +32,46 @@ define(function(require, exports, module) {
     var Share = require('plugins/share/1.0.0/share');
     var share = new Share('#jShare');
 
-    $('#jPraise').click(function(e){
-        
+    //点赞接口处理
+    function clickInterface(url, data, msg) {
+        io.get(url, data, function(res) {
+            //box.ok(msg + '成功');
+        }, function(res) {
+            box.error(res.msg || '网络错误,请重试');
+        });
+    };
+    $('#jNewsPraise').click(function(e){
+        var praise = new Praise(this);
+        if (!Login.isLogin()) {
+            var numBox = $(this).find('span');
+            var number = parseInt(numBox.text());
+            var dataType = $(this).attr('data-dataType');
+            var type = $(this).attr('data-type');
+            var id = $(this).attr('data-id');
+            var data;
+            if ($(this).hasClass("praised")) {
+                data = {
+                    "dataType": dataType,
+                    "type": type,
+                    "id": id
+                }
+                clickInterface($PAGE_DATA['commentClickUrl'], data, '取消点赞');
+                $(this).removeClass("praised");
+                numBox.text(number-1);
+                praise.delete();
+            } else {
+                data = {
+                    "dataType": dataType,
+                    "type": type,
+                    "id": id
+                }
+                clickInterface($PAGE_DATA['commentClickUrl'], data, '点赞');
+                $(this).addClass("praised");
+                numBox.text(number+1);
+                praise.add();
+            }
+        } else {
+            Login.login(window.location.href);
+        }
     });
 });
---
+++ source/js/module/praise/1.0.0/praise.js
@@ -0,0 +1,78 @@
+define(function(require, exports, module) {
+	'use strict';
+	var $ = require('jquery');
+	var EventEmitter = require('lib/core/1.0.0/event/emitter');
+	var Util = require('lib/core/1.0.0/utils/util');
+
+	/* @param selector [dom selector] dom选择器
+	 * @param options [mix] 参数详情见defaults
+	 */
+	function Praise(selector,options){
+		var _this = this;
+		var defaults = {
+			start:{
+				color:"#E94F06"
+			},
+			end:{
+				
+			},
+			duration:1500
+		};
+		_this.el = $(selector);
+		console.log(_this.el.offset(),_this.el.width(),_this.el.height());
+		_this.options = $.extend(true,{},defaults,options);
+		_this._init();
+	};
+
+	//继承自定义事件
+	Util.inherits(Praise, EventEmitter);
+
+	Praise.prototype._init = function(){
+		var _this = this;
+		_this.x = _this.el.offset().left+(_this.el.width()/2);
+		_this.y = _this.el.offset().top;
+	};
+
+	Praise.prototype.add = function() {
+		var _this = this;
+		var $b = $("<b>+1</b>");
+		$b.css({
+			top: _this.y,
+			left: _this.x,
+			position: "absolute",
+			color: _this.options.start.color
+		});
+		$("body").append($b);
+		$b.animate({
+			top: _this.y - _this.el.height(),
+			left: _this.x - (_this.el.width()/4),
+			opacity: 0,
+			"font-size": _this.el.width()/2
+		},  _this.options.duration, function() {
+			$b.remove();
+		});
+	};
+
+	Praise.prototype.delete = function(){
+		var _this = this;
+		var $b = $("<b>-1</b>");
+		$b.css({
+			top: _this.y,
+			left: _this.x,
+			position: "absolute",
+			color: _this.options.start.color
+		});
+		$("body").append($b);
+		$b.animate({
+			top: _this.y - _this.el.height(),
+			left: _this.x - (_this.el.width()/4),
+			opacity: 0,
+			"font-size": _this.el.width()/2
+		}, _this.options.duration, function() {
+			$b.remove();
+		});
+	};
+
+	module.exports = Praise;
+
+});
