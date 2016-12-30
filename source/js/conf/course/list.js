61908ca14507c102660fab2ce43971eccbb68e75
wangliang <wangliang@zhongzhihui.com>
2016-12-30 14:57:24 +0800

    wl

--- source/js/conf/course/list.js
+++ source/js/conf/course/list.js
@@ -10,7 +10,7 @@ define(function(require, exports, module) {
     var Footer = require('module/footer/1.0.0/footer');
     var topSearch = new TopSearch();
     var loginStatus = new LoginStatus();
-    var fixBar = new FixBar();
+    //var fixBar = new FixBar();
     var footer = new Footer();
     /*顶部搜索、登录状态、底部、右侧在线客服 end*/
 
@@ -19,4 +19,8 @@ define(function(require, exports, module) {
     var io = require('lib/core/1.0.0/io/request');
     //弹出层
     var box = require('lib/ui/box/1.0.1/crossbox');
+
+    $('.stageShow li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
 });
